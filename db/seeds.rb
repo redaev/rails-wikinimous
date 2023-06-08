69fa8a5a65a6b6814875ea8cacc82ca088316f13
Reda Boukakiou <reda_boukakiou@hotmail.com>
2023-06-08 22:37:34 +0200

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+articles = Article.create([
+  { title: 'Article 1', content: 'Un' },
+  { title: 'Article 2', content: 'Deux' },
+  { title: 'Article 3', content: 'Trois' },
+  { title: 'Article 4', content: 'Quatre' },
+  { title: 'Article 5', content: 'Cinq' },
+  { title: 'Article 6', content: 'Six' },
+  { title: 'Article 7', content: 'Sept' },
+  { title: 'Article 8', content: 'Huit' },
+  { title: 'Article 9', content: 'Neuf' },
+  { title: 'Article 10', content: 'Dix' }
+])
